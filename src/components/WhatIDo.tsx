b7fe19b74e309d4216598188e34fff765695e9a2
bhuvan-somisetty <somisettybhuvan5@gmail.com>
2026-09-13 16:31:12 +0530

    feat: prioritize Figma, Adobe, UI Design, Wireframing in CREATE skillset tags

--- src/components/WhatIDo.tsx
+++ src/components/WhatIDo.tsx
@@ -138,17 +138,17 @@ const WhatIDo = () => {
               </p>
               <h5>Skillset & tools</h5>
               <div className="what-content-flex">
+                <div className="what-tags">Figma</div>
+                <div className="what-tags">Adobe</div>
+                <div className="what-tags">UI Design</div>
+                <div className="what-tags">Wireframing</div>
                 <div className="what-tags">Blender</div>
                 <div className="what-tags">Zbrush</div>
-                <div className="what-tags">UI Design</div>
                 <div className="what-tags">Motion</div>
                 <div className="what-tags">Rigging</div>
                 <div className="what-tags">3D Animation</div>
                 <div className="what-tags">Character Design</div>
                 <div className="what-tags">Modelling</div>
-                <div className="what-tags">Figma</div>
-                <div className="what-tags">Adobe</div>
-                <div className="what-tags">Wireframing</div>
               </div>
               <div className="what-arrow"></div>
             </div>
